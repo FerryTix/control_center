db85374011273449eca600ba1ac03d82dfa19f30
Felix Wortmann <felixwortmann@protonmail.com>
2021-02-20 17:03:33 +0100

    inital layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
+import 'package:control_center/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 void main() {
   runApp(MyApp());
 }
 
+const String ACCESS_CODE_KEY = "FTCC_ACCESS_CODE";
+
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -14,9 +17,57 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class WelcomePage extends StatelessWidget {
+class WelcomePage extends StatefulWidget {
+  @override
+  _WelcomePageState createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  String code = "";
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Fähr Trade Control Center"),
+      ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Expanded(
+                child: TextField(
+                  decoration: InputDecoration(labelText: "Bitte Text eingeben"),
+                  onChanged: (String newText) {
+                    setState(() {
+                      code = newText;
+                    });
+                  },
+                ),
+              ),
+              TextButton(
+                onPressed: () async {
+                  await saveCode(code);
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (_) => HomePage(),
+                    ),
+                  );
+                },
+                child: Icon(Icons.check),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> saveCode(final String code) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    //todo code aufbereiten
+    return prefs.setString(ACCESS_CODE_KEY, code);
   }
 }
